5bfe8af8a498d3e3175edfd94744d8cf88e25c55
Duarte <duartepena8@gmail.com>
2026-05-05 10:48:06 +0100

    Delete es directory

--- es/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	
-	if (isset($_SESSION['id_user']))
-		session_destroy();
-	
-	// apos o fim da sessao, o utilizador sera redirecionado para a pagina de login
-	header('refresh: 0; url = ./login.php');
-?>
